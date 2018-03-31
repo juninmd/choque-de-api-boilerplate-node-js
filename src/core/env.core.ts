43b4faf013b6218528f59c53b775e8d7629f1302
Antonio Carlos <jr_acn@yahoo.com.br>
2018-03-31 13:04:02 -0300

    better env path

--- src/core/env.core.ts
+++ src/core/env.core.ts
@@ -1,10 +1,12 @@
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 
-let path = `.env-${process.env.NODE_ENV || 'test'}`
-let exist = fs.existsSync(path)
+const env = (process.env.NODE_ENV || 'test');
+const path = `env/.${env}.env`
+const exist = fs.existsSync(path)
 if (!exist) {
-    console.error(`[API] please create .env file in (${path})`)
+    console.error(`[API] please create file '.${env}' in env/`)
+    console.error(`[API] check the example 'env/sample.env'`)
     process.exit(0);
 }
 else {
